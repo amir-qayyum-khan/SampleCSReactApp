6387a2c71430f2eb5b1e064bee987f9c918ab787
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2018-07-29 19:00:00 +0500

    added fetch common words from company components

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
+import ListOfCompanies from './ListOfCompanies';
 
 export class FetchData extends Component {
   displayName = FetchData.name
 
   constructor(props) {
     super(props);
-    this.state = { forecasts: [], loading: true };
+    this.state = { commonWords: [], loading: true };
 
-    fetch('api/SampleData/WeatherForecasts')
+    fetch('api/SampleData/ProcessWords')
       .then(response => response.json())
       .then(data => {
-        this.setState({ forecasts: data, loading: false });
+        this.setState({ commonWords: data, loading: false });
       });
   }
 
-  static renderForecastsTable(forecasts) {
+  static renderCommonWordsTable(commonWords) {
     return (
       <table className='table'>
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
+            <th>Word</th>
+            <th>Count</th>
+            <th>Companies</th>
           </tr>
         </thead>
         <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
+          {commonWords.map(commonWord =>
+            <tr key={commonWord.word}>
+              <td>{commonWord.word}</td>
+              <td>{commonWord.count}</td>
+              <td><ListOfCompanies list={commonWord.companies} /></td>
             </tr>
           )}
         </tbody>
@@ -42,12 +41,12 @@ export class FetchData extends Component {
   render() {
     let contents = this.state.loading
       ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+      : FetchData.renderCommonWordsTable(this.state.commonWords);
 
     return (
       <div>
-        <h1>Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
+        <h1>Common words within the Company field</h1>
+        <p>Identify the five most common words within the Company field.</p>
         {contents}
       </div>
     );
---
+++ ClientApp/src/components/ListOfCompanies.js
@@ -0,0 +1,41 @@
+﻿import React, { Component } from 'react';
+import Dialog from 'react-dialog';
+
+export default class ListOfCompanies extends Component {
+  displayName = ListOfCompanies.name
+
+  constructor(props) {
+    super(props);
+    this.state = { isDialogOpen: false};
+  }
+
+  openDialog = () => this.setState({ isDialogOpen: true })
+ 
+  handleClose = () => this.setState({ isDialogOpen: false })
+
+  render() {
+    const { list = []} = this.props;
+    const companiesList = list.map(element => <li>{element}</li>);
+    return (
+      <div> 
+        <button type="button" onClick={this.openDialog}>Open List</button>  
+        {
+          this.state.isDialogOpen &&
+          <Dialog
+            title="List Of Comapnies"
+            modal={true}
+            onClose={this.handleClose}
+            buttons={
+              [{
+                 text: "Close",
+                 onClick: () => this.handleClose()
+              }]
+            }>
+              <ul>{companiesList}</ul>
+              <br/>
+          </Dialog>
+        }
+      </div>
+    );
+  }
+}
